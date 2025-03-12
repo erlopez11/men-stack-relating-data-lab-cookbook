e6656109eb31b53f3a35035e4374ef218342c800
erlopez11 <lopezelainy@gmail.com>
2025-03-12 18:27:14 -0500

    wip: require & mount passUserToView & isSignedIn & fix foodController to foodsController

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
-const foodController = require('./controllers/foods.js');
+const foodsController = require('./controllers/foods.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -29,6 +31,8 @@ app.use(
   })
 );
 
+app.use(passUserToView);
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -45,7 +49,9 @@ app.get('/vip-lounge', (req, res) => {
 
 app.use('/auth', authController);
 
-app.use('/users/:userID/foods', foodController);
+app.use(isSignedIn);
+
+app.use('/users/:userID/foods', foodsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
