f84afde40e8f9b8c359d7dbb7ce9f3dd7d3e25e8
erlopez11 <lopezelainy@gmail.com>
2025-03-12 18:49:13 -0500

    wip: add route to user pantry

--- controllers/foods.js
+++ controllers/foods.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-
+//GET /user/:userID/foods
+router.get('/', (req, res) => {
+    res.render('foods/index.ejs');
+});
 
 
 module.exports = router;
