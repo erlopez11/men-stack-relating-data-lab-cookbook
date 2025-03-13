7a0d1763dbed66fec51cd206316bba76155982a3
erlopez11 <lopezelainy@gmail.com>
2025-03-13 18:26:19 -0500

    wip: add delete ind item route

--- controllers/foods.js
+++ controllers/foods.js
@@ -48,5 +48,20 @@ router.get('/:itemId', async (req, res) => {
     }
 });
 
+//Delete /users/:userId/foods/:itemId
+router.delete('/:itemId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.id(req.params.itemId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+        
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+
+});
+
 
 module.exports = router;
