b6a56412eaa4e7e21466c25e1f989c6d80440419
erlopez11 <lopezelainy@gmail.com>
2025-03-12 21:11:50 -0500

    wip: add route to get new form & create new pantry item

--- controllers/foods.js
+++ controllers/foods.js
@@ -7,5 +7,22 @@ router.get('/', (req, res) => {
     res.render('foods/index.ejs');
 });
 
+// GET /user/:userId/foods/new
+router.get('/new', async (req, res) => {
+    res.render('foods/new.ejs');
+});
+
+//POST /users/:userId/foods
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+})
 
 module.exports = router;
