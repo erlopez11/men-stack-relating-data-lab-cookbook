94c4d9d53e331f1fc9aadae219818aeb1bd8308a
erlopez11 <lopezelainy@gmail.com>
2025-03-14 09:51:51 -0500

    wip: require and mount user schema

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/foods.js');
+const usersController = require('./controllers/users.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -39,19 +40,13 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
 
 app.use('/auth', authController);
 
 app.use(isSignedIn);
 
 app.use('/users/:userID/foods', foodsController);
+app.use('/users/:userId/community', usersController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
